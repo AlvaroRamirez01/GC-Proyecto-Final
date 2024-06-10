67f88ab3c4e676fc580deb443facf4918c39e1d6
Ethan Ortega <ethan@ciencias.unam.mx>
2024-06-09 22:18:58 -0600

    actualizadas dependencias del proyecto

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule ProyectoFinal.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:libgraph, "~> 0.13"}
+      {:libgraph, "~> 0.16.0"},
+      {:gnuplot, "~> 1.22"}
     ]
   end
 end
